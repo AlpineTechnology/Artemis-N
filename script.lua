f4400efd4bdb0ec4296cf5bbe4ccf0984ef053b9
kura <89491373+kuraise@users.noreply.github.com>
2022-11-25 23:32:14 -0500

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,12 @@
+local rbxmSuite = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/rbxm-suite/master/src/rbxm-suite.lua"))()
+local getRel = rbxmSuite.download("AlpineTechnology/Artemis-N@latest", "Artemis.N.-.v1.0.0.rbxm")
+local project = rbxmSuite.launch(getRel, {runscripts = true})
+
+--[[
+FUNCTIONS:
+shared.Add(args: table) -- Add Themes
+shared.Set(theme_name: string) -- Sets the Theme
+
+CURRENT OFFICIAL THEMES:
+"EggShell", "EggShellInv"
+]]
