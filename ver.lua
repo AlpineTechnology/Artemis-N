fc9dd6f88972f76ecaddb281ae8dece14915dbc0
kura <89491373+kuraise@users.noreply.github.com>
2022-12-05 16:54:25 -0500

    Update ver.lua

--- ver.lua
+++ ver.lua
@@ -1,1 +1,16 @@
-return "v1.1.1"
+-- credits to RBXM-Suite for the idea...
+local httpService = game:GetService("HttpService")
+
+local function httpGet(url)
+	local response, code = game:HttpGetAsync(url)
+
+	return assert(response, "Error " .. tostring(code) .. ": Failed to GET from " .. url)
+end
+
+local function fetch()
+    local resp = httpGet("https://api.github.com/repos/AlpineTechnology/Artemis-N/releases/latest")
+
+    return httpService:JSONDecode(resp).tag_name
+end
+
+return fetch()
